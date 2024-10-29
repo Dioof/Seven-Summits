c4b4b1683261de70252abc13ab6ce12e4eec3d38
D one i <151202143+Dioof@users.noreply.github.com>
2024-10-29 21:07:28 +0700

    Delete Seven-Summits directory

--- Seven-Summits/Main/nav-bar.js
+++
@@ -1,8 +0,0 @@
-$(function() {  
-  $(".sub-menu-link").click(function(e) {
-    e.preventDefault();    
-    $(".sub-menu-overlay").toggleClass("open");
-    $(".sub-menu").toggleClass("open");
-    $("body").toggleClass("cover");
-  });	
-});
